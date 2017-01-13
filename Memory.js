0cb8494b730df03fa825cdd5ac1b33181bf3d1ac
Johan Steenkamp <ovalcode@users.noreply.github.com>
2017-01-13 12:15:49 +0200

    Added PPU memory population functionality 	modified:   Memory.js 	modified:   PPU.js 	modified:   index.html

--- Memory.js
+++ Memory.js
@@ -1,7 +1,8 @@
-function memory()
+function memory(ppu)
 
 {
   var mainMem = new Uint8Array(65536);
+  var localPPU = ppu;
 
   this.attachCartridge = function(file, cpu) {
        var reader = new FileReader();
@@ -22,17 +23,23 @@ function memory()
   }
 
   this.readMem = function (address) {
-    if ((address > 0x2000)  & (address < 0x8000))
-      console.log("Read " + address.toString(16));
-    return mainMem[address];
+    //if ((address > 0x2000)  & (address < 0x8000))
+    //  console.log("Read " + address.toString(16));
+    if ((address >= 0x2000)  & (address < 0x2008))
+      return localPPU.readRegister(address)
+    else
+      return mainMem[address];
   }
 
   this.writeMem = function (address, byteval) {
-    if ((address >= 0x2000) & (address != 0x2002))
-      console.log("Write " + address.toString(16) + " " + byteval.toString(16));
+    //if ((address >= 0x2000) & (address != 0x2002))
+    //  console.log("Write " + address.toString(16) + " " + byteval.toString(16));
     if (address >= 0x8000)
       return;
-    mainMem[address] = byteval;
+    if ((address >= 0x2000)  & (address < 0x2008))
+      localPPU.writeRegister(address, byteval)
+    else
+      mainMem[address] = byteval;
   }
 
 }
--- PPU.js
+++ PPU.js
@@ -2,6 +2,9 @@ function ppu(screenCanvas) {
   var contextScreen = screenCanvas.getContext("2d");
   var screenData = contextScreen.createImageData(256, 240);
   var screenDataAsArray = screenData.data;
+  var registers = new Uint8Array(8);
+  var ppuMemory = new Uint8Array(65536);
+  var writeCounter = 0;
 
   this.draw = function () {
     var i;
@@ -13,5 +16,21 @@ function ppu(screenCanvas) {
     }
 
     contextScreen.putImageData(screenData,0,0);
+  }
+
+  this.readRegister = function(address) {
+    return registers[address & 7];
+  }
+
+  this.writeRegister = function(address, value) {
+    registers[address & 7] = value & 0xff;
+    if (address == 0x2006) {
+      writeCounter = (writeCounter << 8) | (value & 0xff);
+      writeCounter = writeCounter & 0xffff;
+    } else if (address == 0x2007) {
+      ppuMemory [writeCounter] = value;
+      writeCounter++;
+      writeCounter = writeCounter & 0xffff;
+    }
   }
 }
